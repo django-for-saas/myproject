9bafc08b587f8c010a6a4a9f18e2a755956b50c1
Kostja P <kpavlovsky@users.noreply.github.com>
2023-09-26 07:16:27 +0200

    cleanup

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User  # Assuming you're using Django's built-in User model for authentication
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,18 +14,20 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     order_date = models.DateTimeField(auto_now_add=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    # Add more fields for order details as needed
+
 
 class SupportTicket(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, default='Open')  # Options: Open, In Progress, Closed
+    status = models.CharField(max_length=20, default='Open')
+
 
 class SupportTicketMessage(models.Model):
     ticket = models.ForeignKey(SupportTicket, on_delete=models.CASCADE)
@@ -30,11 +35,12 @@ class SupportTicketMessage(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
 class CustomerPayment(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     payment_date = models.DateTimeField(auto_now_add=True)
-    payment_method = models.CharField(max_length=50)  # e.g., Credit Card, PayPal, etc.
+    payment_method = models.CharField(max_length=50)
 
     def __str__(self):
         return f"${self.amount} Payment by {self.customer.name} on {self.payment_date}"
